dd7c0c7c132ec579b2873e1a803d6b271911a7cb
Miller Rich <william.miller.rich@gmail.com>
2020-09-23 21:55:05 -0500

    require connection in orm.js, comment basic layout for orm.js

---
+++ config/orm.js
@@ -0,0 +1,9 @@
+var connection = require("./connection");
+
+// var orm = {
+//     selectAll: function()
+//     insertOne: function()
+//     updateOne: function()
+// };
+
+// module.exports = orm;
