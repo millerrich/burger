8d0b41b66e53369434d15a3fea9c2f6997ff8df1
Miller Rich <william.miller.rich@gmail.com>
2020-09-23 19:43:53 -0500

    add seeds to seeds.sql and run schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ("cheeseburger");
+INSERT INTO burgers (burger_name)
+VALUES ("hamburger");
+INSERT INTO burgers (burger_name)
+VALUES ("veggie burger");
